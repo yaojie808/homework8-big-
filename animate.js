251f2fca3e2e5e1f7e3496c8bd645fc327a8e65c
Zhaoyunhui0920 <379854182@qq.com>
2019-12-18 22:43:35 +0800

    1

--- animate.js
+++ animate.js
@@ -1,39 +1,51 @@
-function getStyle(obj,attr){
-	if(obj.currentStyle){
-		return obj.currentStyle[attr];
-	}else{
-		return getComputedStyle(obj,null)[attr];
+window.οnlοad=function(){ 
+	function getStyle(obj,attr){
+		if(obj.currentStyle){
+			return obj.currentStyle[attr];
+		}else{
+			return getComputedStyle(obj,null)[attr];
+		}
 	}
-}
 
-function animate(obj,json,callback){
-	clearInterval(obj.timer);
-	obj.timer = setInterval(function(){
-		var isStop = true;
-		for(var attr in json){
-			var now = 0;
-			if(attr == 'opacity'){
-				now = parseInt((getStyle(obj,attr)*100));
-			}else{
-				now = parseInt(getStyle(obj,attr));
+	function animate(obj,json,callback){
+		
+		obj.timer = setInterval(function(){
+			var isStop = true;
+			for(var attr in json){
+				var now = 0;
+				if(attr == 'opacity'){
+					now = parseInt((getStyle(obj,attr)*100));
+				}else{
+					now = parseInt(getStyle(obj,attr));
+				}
+				var speed = (json[attr] - now)/6;
+				speed = speed>0?Math.ceil(speed):Math.floor(speed);
+				var current = now + speed
+				if(attr == 'opacity'){
+					obj.style.opacity = current/100 + "px";
+				}else{
+					obj.style[attr]= current + "px";
+				}
+				// if(attr == 'opacity'){
+				// 	box2.style.opacity = current/100 + "px";
+				// }else{
+				// 	box2.style[attr]= current + "px";
+				// }
+				// if(attr == 'opacity'){
+				// 	box3.style.opacity = current/100 + "px";
+				// }else{
+				// 	box3.style[attr]= current + "px";
+				// }
+				
+				if(json[attr] !== current){
+					isStop = false;
+				}
 			}
-			var speed = (json[attr] - now)/6;
-			speed = speed>0?Math.ceil(speed):Math.floor(speed);
-			var current = now + speed
-			if(attr == 'opacity'){
-				box.style.opacity = current/100 + "px";
-			}else{
-				box.style[attr]= current + "px";
+			if(isStop){
+				clearInterval(obj.timer);
 			}
-			
-			if(json[attr] !== current){
-				isStop = false;
-			}
-		}
-		if(isStop){
-			clearInterval(obj.timer);
-		}
-	},60)
+		},60)
+	}
 }
 
 	//匀速运动
